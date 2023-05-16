59fbe736e020085eebf9e60a50fbb80af0fd8666
Lapa <lapabig@gmail.com>
2023-05-16 18:13:34 +0600

    Updated

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int leap;
+        if (year %4 == 0 && year % 100 != 0 || year%400==0){
+            leap = 2;
+        } else {
+            leap = 1;
+        }
+
+        switch (leap){
+            case (1):
+                System.out.println("not leap");
+                break;
+            case (2):
+                System.out.println("leap");
+                break;
+            default: System.out.println("wrong number!");
+        }
     }
 }
